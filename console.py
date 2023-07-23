6bcc7595e7816786b9736057392d789ba2313408
James Bawa <jbawa032@gmail.com>
2023-07-23 13:49:27 -0700

    all good

--- console.py
+++ console.py
@@ -3,19 +3,21 @@
 
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         ''' method to exit the cli with ctrl + d in linux
         and ctrl + c in windows '''
 
         return True
 
-    def do_quit(self, line):
+    def do_quit(self, arg):
         ''' method to quit the cli() '''
 
         return True
@@ -23,5 +25,26 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
+    def do_create(self, arg):
+
+        if arg:
+            if arg == 'BaseModel':
+                new_obj = BaseModel()
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_show(self, arg):
+        
+        if arg:
+            args = arg.split()
+            if len(args) == 0:
+                print("** class name missing **")
+            elif len(args) == 1:
+                print("** instance id missing **")
+            else:
+                clsName = args[0]
+                idName = args[1]
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
